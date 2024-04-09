ba48ed0bcb355840fd222f89faac320184f5edf8
lucasvazz16 <lucasmvazz@gmail.com>
2024-04-09 22:04:23 +0000

    funciones para la conexion

---
+++ kernel/src/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<commons/log.h>
+#include<commons/string.h>
+#include<commons/config.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+
+#include <utils/conexion.h>
+
+#define PUERTO_KERNEL_SERVER "35009"
+#endif
