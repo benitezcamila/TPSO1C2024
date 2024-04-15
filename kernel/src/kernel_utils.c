7b34c7a37ace28199110d94914cb8fc8229a1672
FedeeHygonenq <federicohygonenq08@gmail.com>
2024-04-15 21:37:00 +0000

    prototipoE-S

--- kernel/src/kernel_utils.c
+++ kernel/src/kernel_utils.c
@@ -1,18 +1,5 @@
-#include "main.h"
-
-#include <bits/pthreadtypes.h>
-#define PUERTO_ESCUCHA 300021
-#define IP_MEMORIA ""
-#define PUERTO_MEMORIA ""
-
-#define IP_CPU ""
-#define PUERTO_CPU_DISPATCH ""
-#define PUERTO_CPU_INTERRUPT ""
-
-int iniciar_kernel()
-{
-    return iniciar_servidor(PUERTO_ESCUCHA, "KERNEL");
-}
+#include "kernel_utils.h"
+#include <pthread.h>
 
 void procesos_kernel()
 {
@@ -21,35 +8,36 @@ void procesos_kernel()
 
     pthread_create(&thread_memoria,
                    NULL,
-                   (void *)conexion_memoria(),
+                   (void *)conexion_memoria,
                    NULL);
 
-    pthread_detach(thread_memoria);
-
+    
     pthread_create(&thread_cpu_I,
                    NULL,
-                   (void *)conexion_cpu_I(),
+                   (void *)conexion_cpu_I,
                    NULL);
 
-    pthread_detach(thread_cpu_I);
     pthread_create(&thread_cpu_D,
                    NULL,
-                   (void *)conexion_cpu_D(),
+                   (void *)conexion_cpu_D,
                    NULL);
+    pthread_detach(thread_memoria);
+
+    pthread_detach(thread_cpu_I);
 
     pthread_detach(thread_cpu_D);
 }
 
 void conexion_memoria()
 {
-    int fd_memoria = crear_conexion(IP_MEMORIA, PUERTO_MEMORIA);
+    int fd_memoria = crear_conexion(configuracion.IP_MEMORIA, configuracion.PUERTO_MEMORIA);
     int cod_op;
     log_info(logger_kernel, "Conectado-Kernel-memoria");
     send(fd_memoria, &cod_op, sizeof(int), MSG_WAITALL);
 }
 void conexion_cpu_I()
 {
-    int fd_cpu_distpach = crear_conexion(IP_CPU, PUERTO_CPU_DISPATCH);
+    int fd_cpu_distpach = crear_conexion(configuracion.IP_CPU, configuracion.PUERTO_CPU_DISPATCH);
     log_info(logger_kernel, "CONEXION CPU-distpach ");
     int cod_op;
     send(fd_cpu_distpach, &cod_op, sizeof(int), MSG_WAITALL);
@@ -57,7 +45,7 @@ void conexion_cpu_I()
 
 void conexion_cpu_D()
 {
-    int fd_cpu_interrupt = crear_conexion(IP_CPU, PUERTO_CPU_INTERRUPT);
+    int fd_cpu_interrupt = crear_conexion(configuracion.IP_CPU, configuracion.PUERTO_CPU_INTERRUPT);
     log_info(logger_kernel, "CONEXION CPU-interrupt ");
     int cod_op;
     send(fd_cpu_interrupt, &cod_op, sizeof(int), MSG_WAITALL);
---
+++ kernel/src/kernel_utils.h
@@ -0,0 +1,10 @@
+#include "main.h"
+
+#include <bits/pthreadtypes.h>
+
+
+void conexion_cpu_D();
+
+void conexion_memoria();
+
+void conexion_cpu_I();
