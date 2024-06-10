b173b143ebdb457e28b5bb3d6d4d040d6eff4c21
Marco Szapari Marcos <mszaparimarcos@frba.utn.edu.ar>
2024-06-09 21:11:21 -0300

    Avances en el ciclo de instrucciones. + correcciones.

--- cpu/src/ciclo_de_instruccion.h
+++ cpu/src/ciclo_de_instruccion.h
@@ -4,36 +4,14 @@
 #include <utils/estructuras.h>
 #include <commons/string.h>
 
-extern registros_CPU* contextoRegistros;
+extern registros_CPU* contexto_registros;
 extern char* linea_de_instruccion;
 extern const short max_long_instruccion = 6;
 extern char* linea_de_instruccion_tokenizada[max_long_instruccion];
 extern int llego_interrupcion;
 extern tipo_de_interrupcion motivo_interrupcion;
 
-/*Comandos que nos piden
-typedef enum{
-    SET, MOV_IN, MOV_OUT, SUM, SUB, JNZ,
-    RESIZE, COPY_STRING, WAIT, SIGNAL,
-    IO_GEN_SLEEP,
-    IO_STDIN_READ,
-    IO_STDOUT_WRITE,
-    IO_FS_CREATE,
-    IO_FS_DELETE,
-    IO_FS_TRUNCATE,
-    IO_FS_WRITE,
-    IO_FS_READ,
-    EXIT
-} t_comando;
-*/
-//TLB
-typedef struct {
-    int pid;
-    int pagina;
-    int marco;
-} tlb;
-
-void mmu(uint32_t, uint32_t*, uint32_t*);
+void mmu(t_TLB, t_tabla_paginas*, uint32_t, uint32_t, uint32_t);
 void fetch_instruction();
 void decode();
 void execute();
